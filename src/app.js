2e5ff130935d13a93b76443ee53d24262c19a14a
Cristhyan <cristhyan.ko@outlook.com>
2018-10-30 13:47:20 -0400

    Correções

--- src/app.js
+++ src/app.js
@@ -5,17 +5,30 @@ import Search from './components/search';
 import UserInfo from './components/user-info';
 import Actions from './components/actions';
 import Repos from './components/repos';
+import Header from './components/header';
 
 class App extends Component {
     render() {
         return (
             <div className='app'>
                 <div className='container'>
+                    <Header />
                     <Search />
                     <UserInfo />
                     <Actions />
                     <div className='row'>
-                        <Repos className='repos' title='Repositórios' repos={[{ name: 'nome', link: 'link' }]} />
+                        <Repos className='repos' title='Repositórios' repos={
+                                                        [
+                                                            { name: 'Nome do repositório',
+                                                              link: 'Link do repositório' 
+                                                            },
+                                                            { name: 'Nome do repositório 2',
+                                                              link: 'Link do repositório 2' 
+                                                            },
+                                                            { name: 'Nome do repositório 3',
+                                                              link: 'Link do repositório 3' 
+                                                            }
+                                                        ]} />
                         <Repos className='starred' title='Favoritos' repos={[{ name: 'nome', link: 'link' }]} />
                     </div>
                 </div>
---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+
+const Header = () => (
+    <div className='row'>
+        <div className='col-12 header'>
+                    <img width="60" height="60" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Ei-sc-github.svg/200px-Ei-sc-github.svg.png' className='rounded float-left' />
+                    <h1>GitHub APP</h1>
+        </div>
+    </div>
+)
+
+export default Header
